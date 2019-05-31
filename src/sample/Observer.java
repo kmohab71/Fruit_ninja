23a6865ae615a905214cf5cd8a34959c6e25464d
khaledmohab1 <kmohab71@yahoo.com>
2019-05-31 14:07:48 +0200

    final version

--- src/sample/Observer.java
+++
@@ -1,6 +0,0 @@
-package sample;
-
-public abstract class Observer {
-    protected Subject subject;
-    abstract void update();
-}
--- src/sample/mainrun.java
+++
@@ -1,9 +0,0 @@
-package sample;
-
-import org.newdawn.slick.SlickException;
-
-public class mainrun {
-    public static void main(String[] args) throws SlickException {
-        Controller.getInstance().display();
-    }
-}
